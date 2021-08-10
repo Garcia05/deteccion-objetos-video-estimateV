12dd82b0ae44d851308d51968dcb1862dfa54404
Garcia05 <38122940+Garcia05@users.noreply.github.com>
2021-08-10 01:24:22 -0500

    Add files via upload

--- deteccion_video_estimateV.py
+++ deteccion_video_estimateV.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
         cap = cv2.VideoCapture(opt.directorio_video)
         # frame_width = int(cap.get(3))
         # frame_height = int(cap.get(4))
-        out = cv2.VideoWriter('outp.mp4',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1280,960))
+        out = cv2.VideoWriter('outp2.mp4',cv2.VideoWriter_fourcc(*'MP4V'), 10, (1280,960))
     colors = np.random.randint(0, 255, size=(len(classes), 3), dtype="uint8")
     a=[]
     (W, H) = (None, None)
